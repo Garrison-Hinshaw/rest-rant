97b170e1a28de0fc45d2a58f784fa6ebfe9f9d52
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-08-17 00:34:56 -0400

    added mock data for places

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,22 @@
 const router = require('express').Router()
 
 router.get('/', (req,res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Bone on the Cobb',
+        city: 'Kansas City',
+        state: 'Missouri',
+        cuisine: 'Corn related dishes with bones',
+        picture: 'https://place-puppy.com/300x300'
+    },
+    {
+        name: 'Backstreet Burgers',
+        city: 'St. Louis',
+        state: 'Missouri',
+        cuisine: 'Street food',
+        picture: 'https://place-puppy.com/300x300'
+    }]
+    res.render('./places/index', { places })
 })
 
 module.exports = router
+
