9ac8cd2dc2ba3671a71a6b1f127fea7a1dce78cb
Garrison Hinshaw <garrison.hinshaw18@gmail.com>
2022-08-18 20:25:39 -0400

    changes for later use

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
           <Def>
               <main>
                   <h1>HOME</h1>
+                  <div>
+                    <img src="/images/burger.jpg" alt="burger" />
+                  <div>
+                  Photo by <a href="https://unsplash.com/@1ncreased?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Lidye</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                  </div>
+                  </div>
                   <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
